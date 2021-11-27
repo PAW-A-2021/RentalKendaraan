901954cbcd0e3fe386df93cc7fb3646a5da4db8a
FatkhiAlauddin <cupetongspeed12@gmail.com>
2021-11-27 18:06:27 +0700

    Update

--- RentalKendaraan/Models/Customer.cs
+++ RentalKendaraan/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -13,9 +14,14 @@ namespace RentalKendaraan.Models
         }
 
         public int IdCustomer { get; set; }
+        [Required(ErrorMessage = "Nama customer tidak boleh kosong")]
         public string NamaCustomer { get; set; }
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Hanya Boleh Diisi Oleh Angka")]
         public string Nik { get; set; }
+        [Required(ErrorMessage = "Alamat Tidak Boleh Kosong")]
         public string Alamat { get; set; }
+        [MinLength(10, ErrorMessage = "No HP minimal 10 angka")]
+        [MaxLength(13, ErrorMessage = "No HP maksimal 13 angka")]
         public string NoHp { get; set; }
         public int? IdGender { get; set; }
 
